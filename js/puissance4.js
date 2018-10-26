8fa4630f50e2b47e1fa75e88db9b520da1d96e23
sicca <dcl.laurentj@pixel18.fr>
2018-10-26 12:07:50 +0200

    Add Diagramme

--- js/puissance4.js
+++ js/puissance4.js
@@ -7,33 +7,40 @@ function puissance4() {
     var caseOn;
     var caseOff;
 
-    //Déclaration des éléments du DOM
+    //Déclaration des éléments du DOM -------------------------------------------------------------------------------------
+    var caseMorpion = document.getElementsByTagName("td");
 
-    // Création du plateau de jeux en 2 dimensions 7 colonnes x 6 lignes -> new Array();
+    // Création du plateau de jeux en 2 dimensions 7 colonnes x 6 lignes -> new Array();-----------------------------------
+    // Définition de nos 7 colonnes pour choix joueurs---------------------------------------------------------------------
 
-    //TODO: Fonction Jeu
-    //TODO: Choix de Joueur 1 -> joueur1Choix
-    //TODO: Choix de Joueur 2 -> joueur2Choix
+    //TODO: Fonction Jeu---------------------------------------------------------------------------------------------------
+    //TODO: Choix de Joueur 1 -> joueur1Choix------------------------------------------------------------------------------
+    //TODO: Choix de Joueur 2 -> joueur2Choix------------------------------------------------------------------------------
 
-    //TODO: récupérer le choix du Joueur 1 -> joueur1Choix
-    //TODO: récupérer le choix du Joueur 2 -> joueur2Choix
-    // Récupérer le numéro de colonne cliqué
+    //TODO: récupérer le choix du Joueur 1 -> joueur1Choix-----------------------------------------------------------------
+    //TODO: récupérer le choix du Joueur 2 -> joueur2Choix-----------------------------------------------------------------
+    // Récupérer le numéro de colonne cliqué-------------------------------------------------------------------------------
 
-    // Modifier le style de la case en fonction du joueur 1 ou 2 -> Array.slice()
-    // vérifier si case ON ou OFF
-    // vérifier si 4 case ON en vertical, horizontal
+    // Modifier le style de la case en fonction du joueur 1 ou 2 -> Array.slice()------------------------------------------
+    // vérifier si case ON ou OFF------------------------------------------------------------------------------------------
+    // vérifier si 4 case ON en vertical, horizontal-----------------------------------------------------------------------
 
 
-    //TODO: Si : Cas égalité -> Match nul
-    //TODO: Sinon Si : Cas de victoire 
-    //TODO: Sinon : Cas de défaite
-    //TODO: Afficher les résultats
+    //TODO: Si : Cas égalité -> Match nul----------------------------------------------------------------------------------
+    //TODO: Sinon Si : Cas de victoire ------------------------------------------------------------------------------------
+    //TODO: Sinon : Cas de défaite-----------------------------------------------------------------------------------------
+    //TODO: Afficher les résultats-----------------------------------------------------------------------------------------
 
-    //TODO: Fonction restart
+    //TODO: Fonction restart-----------------------------------------------------------------------------------------------
+    function reload() {
+        location.reload();
+    }
 }
 
 puissance4();
 
-var caseMorpion = document.getElementsByTagName("td");
-caseMorpion[41].style.backgroundColor = "red"
+
+// Pour test
+var caseMorpion = document.getElementsByTagName("div")
+caseMorpion[42].style.backgroundColor = "red"
 console.log(caseMorpion);
