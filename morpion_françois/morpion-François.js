300d32b0a0971926a1c64c2eeadc0cd651729333
dcl-jipay <dcl-jeanphig@18pixel.fr>
2018-10-29 11:59:42 +0100

    ajout morpion rançois

---
+++ morpion_françois/morpion-François.js
@@ -0,0 +1,25 @@
+const gameArea = document.getElementById("game-area"); // le seul élément du DOM dont on a besoin c'est l'élément parent de nos cases -> on récupérera l'enfant au clique.
+
+var cellsStatus = [ ["vide", "vide", "vide"],
+    ["vide", "vide", "vide"],
+    ["vide", "vide", "vide"] ];
+
+gameArea.addEventListener("click", function(event) {
+    var clickedCellElmt;
+    var clickedCellName;
+    var clickedCellCoordX;
+    var clickedCellCoordY;
+
+    clickedCellElmt = document.getElementById(event.target.id); //enregistre l'élément  enfant du DOM qui a été cliquée.
+    clickedCellName = event.target.id; //renvoie l'id de la cellule cliquée sous forme de string
+    clickedCellCoordX = parseInt(clickedCellName[1]); // récupère le 2eme caractère dans l'ID
+    clickedCellCoordY = parseInt(clickedCellName[4]); // récupère le 4eme caractère dans l'ID
+    console.log("X : " + clickedCellCoordX + ", Y : " + clickedCellCoordY);
+    console.log("clickedCell : " + clickedCellName);
+
+    cellsStatus[clickedCellCoordX-1][clickedCellCoordY-1] = "clicked"; // modifie le tableau pour stocker la donnée dans la case cliquée
+
+    console.log(cellsStatus);
+
+    clickedCellElmt.innerHTML = "Clicked"; // change la case qui viens d'être cliquée (contrairement a ce que j'ai dis peut être fait aussi avec le clic MAIS n'oubliez pas de stocker l'information dans un tableau car il faudra utiliser le tableau pour vérifier les conditions de victoire
+});
